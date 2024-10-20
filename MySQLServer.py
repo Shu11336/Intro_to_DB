61855f95c98524beb10131756c820d23ddc9fadb
Shu11336 <moazshouman@gmail.com>
2024-10-20 21:26:33 +0300

    Intro to db

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+try:
+        
+    mydb = mysql.connector.connect(
+        host = '127.0.0.1',
+        port = '3306',
+        user = 'root',
+        password = 'Root00root',
+        database = 'alx_book_store',
+    )
+    if mydb.is_connected():
+        #* TEST THE DATABSE IS CONNECTED
+        print('DATABASE AlX_STORE_STORE WORK SUCCESSFULLY!')
+    else:
+
+        #* EXECUTE COMMAND TO DB
+        cursor = mydb.cursor()
+        cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
+
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
